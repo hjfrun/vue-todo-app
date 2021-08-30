37d2d2b0ddf1d11200e0bc1eb3868c0805271d57
hjfrun <hjfrun@gmail.com>
2021-08-30 23:48:20 +0800

    minor

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -44,15 +44,6 @@
     <v-container>
       <router-view></router-view>
     </v-container>
-
-    <!-- <v-snackbar v-model="snackbar.show" timeout="2000">
-      {{ snackbar.text }}
-      <template v-slot:action="{ attrs }">
-        <v-btn color="white" text v-bind="attrs" @click="snackbar.show = false">
-          Close
-        </v-btn>
-      </template>
-    </v-snackbar> -->
   </v-app>
 </template>
 
